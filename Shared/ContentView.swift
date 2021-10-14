c1580c513152f43eade3164035d6edc62e6ccac0
aryanm5 <aryanm.email@gmail.com>
2021-10-14 13:06:10 -0700

    Encoding works with UTF32 result and UInt32 key

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,43 +10,64 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
+    
+    
+    @State private var key: String = "123"
+    @State private var message: String = "abcdefg"
+    @State private var encoded: String = "bababab"
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink(destination: Text("Item at \(item.timestamp!, formatter: itemFormatter)")) {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        ScrollView {
+            VStack {
+                TextField("Key", text: $key)
+                    .border(Color.green)
+                Text("Key: \(key)")
+                
+                TextField("Message", text: $message)
+                    .border(Color.green)
+                Text("Message: \(message)")
+                
+                Button("Encode!") {
+                    xor()
                 }
-                .onDelete(perform: deleteItems)
+                
+                Text("Encoded: \(encoded)")
+                
             }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
         }
     }
-
+    
+    private func xor() {
+        let cipher = UInt32(key)!
+        
+        var encrypted = [UInt32]()
+        
+        // encrypt bytes
+        for t in message {
+            encrypted.append(UInt32(t.unicodeScalarCodePoint()) ^ cipher)//[t.offset % key.count])
+        }
+        
+        let data = Data(bytes: encrypted, count: encrypted.count * MemoryLayout<UInt32>.stride)
+        encoded = String(data: data, encoding: .utf32LittleEndian) ?? "There was an error."
+        
+        //encoded = String(utf32CodeUnits: encrypted, count: encrypted.count)
+        //encoded = String(bytes: encrypted, encoding: .utf16) ?? "There was an error."
+        
+        message = encoded
+        //UIPasteboard.general.setValue(encoded, forPasteboardType: "")
+    }
+    
+    
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
-
+            
             do {
                 try viewContext.save()
             } catch {
@@ -57,11 +78,11 @@ struct ContentView: View {
             }
         }
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            
             do {
                 try viewContext.save()
             } catch {
@@ -80,6 +101,16 @@ private let itemFormatter: DateFormatter = {
     formatter.timeStyle = .medium
     return formatter
 }()
+
+extension Character {
+    func unicodeScalarCodePoint() -> UInt32 {
+        let characterString = String(self)
+        let scalars = characterString.unicodeScalars
+
+        return scalars[scalars.startIndex].value
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
